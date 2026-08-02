58c84922bdb5dec553599cc3864933e4e7a6d3d6
Smit Delwadia <smitdelwadia100@gmail.com>
2026-08-02 14:34:55 -0500

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            Character m = s.charAt(i);
+
+            if (m == '{' || m == '[' || m == '(') {
+                stack.push(m);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                Character t = stack.pop();
+
+                if (t == '{' && m != '}' || t == '[' && m != ']' || t == '(' && m != ')') {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
